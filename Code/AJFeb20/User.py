39e90a3a4b634e06a99d90cead36795741c647f1
arceuscoding <aylaj884@gmail.com>
2026-02-26 14:30:11 -0800

    Turns out my angle was wrong

--- Code/AJFeb20/User.py
+++ Code/AJFeb20/User.py
@@ -16,3 +16,14 @@ class User :
 
     usercursor.execute("CREATE TABLE user (type VARCHAR(9), username VARCHAR(20), password VARCHAR(), custPermissions BOOLEAN, drivPermissions BOOLEAN, restPermissions BOOLEAN)")
     #Just Going with blanket permissions for the classes as they will be the same for all of those within their type
+    #Apparently we don't need to make any databases for this, so I'll be making a stock customer, driver, resturant and maybe admin
+
+
+
+
+class Login :
+    username = "   "
+    password = "   "
+
+    def log(user, passw):
+
