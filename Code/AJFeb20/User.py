199c5a403ec38cf95fa06b45aac015f4b5e6f21b
arceuscoding <aylaj884@gmail.com>
2026-02-26 14:39:17 -0800

    start of new angle

--- Code/AJFeb20/User.py
+++ Code/AJFeb20/User.py
@@ -6,27 +6,20 @@ class User :
     type = "   "
     username = "   "
     password = "  "
-    
-    users = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="group18"
-    )
 
-    usercursor = users.cursor()
-
-    usercursor.execute("CREATE DATABASE users")
-
-    usercursor.execute("CREATE TABLE user (type VARCHAR(9), username VARCHAR(20), password VARCHAR(), custPermissions BOOLEAN, drivPermissions BOOLEAN, restPermissions BOOLEAN)")
-    #Just Going with blanket permissions for the classes as they will be the same for all of those within their type
+    def User(typ, user, passw):
+        type = typ
+        username = user
+        password = passw
+   
     #Apparently we don't need to make any databases for this, so I'll be making a stock customer, driver, resturant and maybe admin
 
 
 
 
 class Login :
-    username = "   "
-    password = "   "
+    
 
-    def log(user, passw):
+    
+        
 
