128fade18b0d31c2f486a301a58151ef73cb1705
akaboda <agbebakuandrea1@gmail.com>
2026-02-28 10:54:12 -0800

    Update auth_services.py

--- fullstack-project/backend/app/services/auth_services.py
+++ fullstack-project/backend/app/services/auth_services.py
@@ -1,15 +1,15 @@
 from schemas.auth_schemas import RegisterRequest
 from fastapi import HTTPException
 
-def validate_registration_input(payload: RegisterRequest) -> None 
-if len(payload.username.strip()) < 3: 
-  raise HTTPException(status_code = 400, detail= "Username must be atleast 3 characters long; retry")
+def validate_registration_input(payload: RegisterRequest) -> None: 
+  if len(payload.username.strip()) < 3: 
+    raise HTTPException(status_code = 400, detail= "Username must be atleast 3 characters long; retry")
 
-email = payload.email.strip()
-if "@" not in email or "." not in email:
-  raise HTTPException(status_code = 400, detail= "Invalid email format; retry")
+  email = payload.email.strip()
+  if "@" not in email or "." not in email:
+    raise HTTPException(status_code = 400, detail= "Invalid email format; retry")
 
-if len(paylod.password) < 8: 
-  raise HTTPException(status_code = 400, detail= "Password must be atleast 8 characters")
-if not any(ch.isdigit() for ch in payload.password):
-  raise HTTPException(status_code=400, detail= "password must contain atleast one number; retry)
+  if len(payload.password) < 8: 
+    raise HTTPException(status_code = 400, detail= "Password must be at least 8 characters")
+  if not any(ch.isdigit() for ch in payload.password):
+    raise HTTPException(status_code=400, detail= "password must contain atleast one number; retry)
