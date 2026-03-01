b3a275e5a5d53fe1d7dea8d4039c5a9307cfef28
akaboda <agbebakuandrea1@gmail.com>
2026-02-28 18:14:46 -0800

    add registration endpoint for auth service

--- fullstack-project/backend/app/routers/auth.py
+++ fullstack-project/backend/app/routers/auth.py
@@ -1,2 +1,9 @@
 from fastapi import APIRouter
+from schemas.auth_schemas import RegisterRequest
+from services.auth_services import register_user
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/Register", status_code = 201)
+def register(payload: RegisterRequest):
+    register_user(payload)
+    return { "message": "User created successfully"}
