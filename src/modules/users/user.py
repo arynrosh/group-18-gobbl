e26e5d010dc35deb0caa9dfdd6b508207429ffb1
arceuscoding <aylaj884@gmail.com>
2026-03-04 12:50:48 -0800

    Finally got it onto a different branch and a rough version of accounts, login and search bar

--- src/modules/users/user.py
+++ src/modules/users/user.py
@@ -1,26 +1,52 @@
-# import mysql.connector
-# #resolve later
+from abc import ABC, abstractmethod
+class User :
+    type = ""
+    username = ""
+    password = ""
+    email = ""
+    payinfo = 0
 
-# from abc import ABC, abstractmethod
-# class User :
-#     type = "   "
-#     username = "   "
-#     password = "  "
+    def __init__(self, typ, user, passw, eml, pyfo):
+        self.type = typ
+        self.username = user
+        self.password = passw
+        self.email = eml
+        self.payinfo = pyfo
 
-#     def User(typ, user, passw):
-#         type = typ
-#         username = user
-#         password = passw
+    
    
-#     #Apparently we don't need to make any databases for this, so I'll be making a stock customer, driver, resturant and maybe admin
+    #Apparently we don't need to make any databases for this, so I'll be making a stock customer, driver, resturant and maybe admin
 
+def getPassword(Person):
+    return Person.password
 
+customer = User("cust", "customer", "badPassword", "cus@gmail.com", 12345)
+driver = User("drv", "driver", "123456", "drv@gmail.com", 98765)
+resturant = User("rest", "resturant", "password", "rest@gmail.com", 34567)
+stockuser = {
+    "customer" ; customer;
+    "driver" ; driver;
+    "resturant" ; resturant
+} 
 
-
-# class Login :
+class Login :
     
-#hahaha
 
-    
-        
+    usrnm = ""
+    eml = ""
+    passw = ""
 
+    def signin():
+        usr = str(input("Username"))
+        pasw = str(input("Password"))
+        if usr:
+            if stockuser.get(usr):
+                if stockuser.get(usr.getPassword()) == pasw:
+                    print("Login Comfirmed")
+                    #right now this just checks that they have an account and correct password
+                else:
+                    print("Password does not match with account")
+            else:
+                print("Account not found")
+        else:
+            print("A username must be entered")
