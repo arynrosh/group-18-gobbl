bcb19cd150f2b39105e3d08eb21244c42bf60b4a
Drea Akabo <agbebakuandrea1@gmail.com>
2026-03-05 09:52:33 -0800

    Register menu search router in main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers.auth import router as auth_router
 from app.routers.restaurant import router as menu_router
 from app.routers.restaurant_search import router as restaurant_search_router
+from app.routers.menu_search import router as menu_search_router
 
 app = FastAPI(
     title="Gobbl Food Delivery API",
@@ -13,6 +14,7 @@ app.include_router(auth_router)
 # everyone should add their routers here
 app.include_router(menu_router)
 app.include_router(restaurant_search_router)
+app.include_router(menu_search_router)
 
 @app.get("/")
 def root():
