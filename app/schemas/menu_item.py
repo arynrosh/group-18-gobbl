8615406b7455365021f96a79eeccc044d3e030fd
Drea Akabo <agbebakuandrea1@gmail.com>
2026-03-11 10:50:35 -0700

    Task 3.2: fix menu search and tests with csv data

--- app/schemas/menu_item.py
+++ app/schemas/menu_item.py
@@ -1,15 +1,8 @@
 from pydantic import BaseModel
 
 class MenuItem(BaseModel):
-    """
-    represents a menu item.
+  
 
-    Attributes: 
-    id (int): unique identifier
-    name (string): menu item name
-    price (float): menu item price
-    restaurant_id (int): ID of the restaurant this item belongs to
-    """
 
     id: int
     name: str
