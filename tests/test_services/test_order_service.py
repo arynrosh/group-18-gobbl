e151ecb09c5ceb6a85d4560eb37d911f166af866
ArceusCoding <aylaj884@gmail.com>
2026-03-14 08:33:55 -0700

    Still won't connect to my methods

--- tests/test_services/test_order_service.py
+++ tests/test_services/test_order_service.py
@@ -1,10 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
+from app.services.order_service import App
+from app.main import app
 from app.schemas.order import Order, OrderItem, Status
 #app\services\order_service.py
-from app.services import order_service
+from app.services.order_service import updateStatus, completeOrderStatus, addToOrder, removeFromOrder, sendOrder
 
-client = TestClient(Order)
+client = TestClient(app)
+client2 = TestClient(App)
 
 #testing methods for status class
 statusTester = Status("1d8e87M")
@@ -24,23 +27,6 @@ foodPrice = 12.99
 foodRid = 13
 orderItemTester = OrderItem(foodName, foodPrice, foodRid)
 
-"""def test_getName():
-    result = orderItemTester.getName()
-    assert result == foodName
-
-def test_getPrice():
-    result = orderItemTester.getPrice()
-    assert result == foodPrice
-
-def test_getRestid():
-    result = orderItemTester.getRestid()
-    assert result == foodRid
-
-def test_toString():
-    result = orderItemTester.toString()
-    expected = "Name: " + foodName + ", price: " + foodPrice + ", resturant ID: " + foodRid
-    assert result == expected"""
-
 #testing methods for order
 orderTester = Order("2f9r98Z", "custest")
 
@@ -52,13 +38,6 @@ def test_removeFromOrder():
     orderTester.addToOrder(orderItemTester)
     orderTester.removeFromOrder(orderItemTester)
     assert any(orderTester.foods, orderItemTester) == False
-
-"""def test_getPrice():
-    orderTester.addToOrder(orderItemTester)
-    orderTester.addToOrder(orderItemTester)
-    expected = foodPrice * 2 #25.98
-    result = orderTester.getPrice()
-    assert expected == result"""
 
 def test_sendOrder():
     result = orderTester.sendOrder()
