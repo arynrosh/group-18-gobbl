07e651e0387db3499b85a3d6c9a3cd5310c97e74
ArceusCoding <aylaj884@gmail.com>
2026-03-15 10:11:28 -0700

    Made test cases into test only instances

--- app/services/order_service.py
+++ app/services/order_service.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 import pytest
 from app.schemas.order import OrderItem, Order, Status
 
@@ -14,83 +15,17 @@ App = FastAPI()
 
 #FastAPI lab presentation 
 
-"""db = "app/data/food_delivery.csv"
-parameters = {"order_id", "food_item", "resturant_id", "order_value"}
-try:
-    response = requests.get(db, parameters)
-    data = response.json()
-except:
-    print("Could not connect to Kaggle database")"""
-
-"""class orderItem:
-
-    def __init__(self, nam: str, pri: int, rID: int):
-        self.name = nam
-        self.price = pri
-        self.restID = rID
-    
-    def getName(self):
-        return self.name
-    
-    def getRestid(self): 
-        return self.restID
-    
-    def getPrice(self):
-        return self.price
-    
-    def toString(self):
-        return "Name: " + self.name + ", price: " + self.price + ", resturant ID: " + self.restID"""
-
 def addToOrder(orderItem: OrderItem) -> Order:
     if Order.sent == False:
         Order.items.append(orderItem)
 
 def removeFromOrder(orderItem: OrderItem) -> Order:
     if Order.sent == False:
-            if any(Order.foods, orderItem):
-                Order.foods.remove(orderItem)
+            if any(Order.items, orderItem):
+                Order.items.remove(orderItem)
 
 def sendOrder(orderId: str) -> Order:
     Order.sent = True
-
-"""class order:
-
-    def __init__(self, oID: str, cNam: str):
-        self.orderID = oID
-        self.custName = cNam
-        self.sent = False
-        self.foods = []
-        self.orderPrice = 0.00
-
-    def add(self, item: orderItem):
-        if self.sent == False:
-            self.foods.append(item)
-            #self.orderPrice = 0.00
-            #self.getPrice()
-    
-    def remove(self, item: orderItem):
-        if self.sent == False:
-            if self.any(self.foods, orderItem):
-                self.foods.remove(orderItem)
-            #self.orderPrice = 0.00
-            #self.getPrice()
-
-    def getPrice(self):
-        for i in self.foods:
-            self.orderPrice += i.price
-        return self.orderPrice
-
-    def showCart(self):
-        for i in self.foods:
-            print(i.toString())
-
-    def sendOrder(self):
-        orderPrice = self.getPrice(self)
-        #billing process
-        #Have it equal a finalPrice
-
-        self.sent = True
-        return self.sent"""
 
 def updateStatus(status: str) -> Status:
     if Status.complete == False:
@@ -99,20 +34,4 @@ def updateStatus(status: str) -> Status:
 def completeOrderStatus() -> Status:
     Status.complete = True
 
-"""class status:
-    def __init__(self, oID: str):
-        self.orderID = oID
-        self.current = "Sent"
-        self.complete = False
-
-    def updateOrder(self, stat: str):
-        if self.complete == False:
-            self.current = stat
-            return self.current
-        else:
-            return "A completed order can not be updated"
-        
-    def complOrd(self):
-        self.complete = True
-        return self.complete"""
 
--- tests/test_services/test_order_service.py
+++ tests/test_services/test_order_service.py
@@ -1,45 +1,48 @@
 import pytest
 from fastapi.testclient import TestClient
+from fastapi import FastAPI
 from app.services.order_service import App
-from app.main import app
+#from app.main import app
 from app.schemas.order import Order, OrderItem, Status
 #app\services\order_service.py
 from app.services.order_service import updateStatus, completeOrderStatus, addToOrder, removeFromOrder, sendOrder
 
-client = TestClient(app)
+#client = TestClient(app)
 client2 = TestClient(App)
 
 #testing methods for status class
-statusTester = Status("1d8e87M")
+def statusTester():
+    return Status("1d8e87M")
 
-def test_updateStatus():
-    statUpdate = "Ready"
+def orderItemTester():
+    foodName = "Tacos"
+    foodPrice = 12.99
+    foodRid = 13
+    return OrderItem(foodName, foodPrice, foodRid)
+
+def orderTester():
+    return Order("2f9r98Z", "custest")
+
+
+def test_updateStatus(statusTester):
+    statusUpdate = "Ready"
     result = statusTester.updateStatus("Ready")
-    assert statUpdate == result
+    assert statusUpdate == result
 
-def test_completeOrderStatus():
+def test_completeOrderStatus(statusTester):
     result = statusTester.completeOrderStatus()
     assert result == True
 
-#testing methods for orderItem
-foodName = "Tacos"
-foodPrice = 12.99
-foodRid = 13
-orderItemTester = OrderItem(foodName, foodPrice, foodRid)
-
-#testing methods for order
-orderTester = Order("2f9r98Z", "custest")
-
-def test_addToOrder():
+def test_addToOrder(orderTester, orderItemTester):
     orderTester.addToOrder(orderItemTester)
     assert any(orderTester.foods, orderItemTester)
 
-def test_removeFromOrder():
+def test_removeFromOrder(orderTester, orderItemTester):
     orderTester.addToOrder(orderItemTester)
     orderTester.removeFromOrder(orderItemTester)
     assert any(orderTester.foods, orderItemTester) == False
 
-def test_sendOrder():
+def test_sendOrder(orderTester):
     result = orderTester.sendOrder()
     assert result
 
