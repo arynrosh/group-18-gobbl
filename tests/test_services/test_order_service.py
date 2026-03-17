0dc3ebea7688e60e4f7ce6d855f7545c925a7393
ArceusCoding <aylaj884@gmail.com>
2026-03-16 19:24:35 -0700

    Testing

---
+++ tests/test_services/test_order_service.py
@@ -0,0 +1,75 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import FastAPI
+from app.services.order_service import App
+from app.schemas.Order import Order, OrderItem, Status
+#app\services\order_service.py
+from app.services.order_service import updateStatus, getStatusCurrent, completeOrderStatus, getStatusComplete, addToOrder, removeFromOrder, sendOrder, getOrderSent, getOrderItems
+
+client2 = TestClient(App)
+
+@pytest.fixture
+def statusTester():
+    return Status(
+        order_id="1234567",
+        current="sent",
+        complete=False,
+    )
+
+@pytest.fixture
+def orderItemTester():
+    return OrderItem(
+        food_item = "Tacos",
+        quantity = 1,
+        order_value = 12.99,
+        resturant_id = 13
+    )
+
+@pytest.fixture
+def orderItemBackup():
+    return OrderItem(
+        food_item = "Cheese",
+        quantity = 2,
+        order_value = 6.66,
+        resturant_id = 13
+    )
+
+@pytest.fixture
+def orderTester():
+    return Order(
+        order_id = "123456",
+        customer_id = "gertrude",
+        restaurant_id = 13,
+        items = [],
+        sent = False
+    )
+
+
+def test_updateStatus(statusTester):
+    statusUpdate = "Ready"
+    updateStatus(statusTester, "Ready")
+    result = getStatusCurrent(statusTester)
+    assert statusUpdate == result
+
+def test_completeOrderStatus(statusTester):
+    completeOrderStatus(statusTester)
+    result = getStatusComplete(statusTester)
+    assert result == True
+
+def test_addToOrder(orderTester, orderItemTester):
+    addToOrder(orderTester, orderItemTester)
+    assert orderTester.items[0] == orderItemTester
+
+def test_removeFromOrder(orderTester, orderItemTester, orderItemBackup):
+    addToOrder(orderTester, orderItemTester)
+    removeFromOrder(orderTester, orderItemTester)
+    #addToOrder(orderTester, orderItemBackup)
+    assert orderTester.items[0] != orderItemTester
+
+def test_sendOrder(orderTester):
+    sendOrder(orderTester)
+    result = orderTester.sent
+    assert result == True
+
+#Copy of command to just run my tests for ease
+#pytest tests/test_services/test_order_service.py
