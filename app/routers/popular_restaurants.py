edfab21193bd6847e4256e970b48d7831d21a4a0
Drea Akabo <agbebakuandrea1@gmail.com>
2026-03-22 10:42:56 -0700

    add popular restaurants by rating endpoint

--- app/routers/popular_restaurants.py
+++ app/routers/popular_restaurants.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query
-from app.services.popular_restaurants_service import get_popular_restaurants_by_orders
+from app.services.popular_restaurants_service import ( get_popular_restaurants_by_orders, get_popular_restaurants_by_rating)
 
 router = APIRouter(prefix="/statistics", tags=["statistics"])
 
@@ -9,3 +9,9 @@ def popular_by_orders(limit: int = Query(default=10, ge=1)):
     Returns the most popular restaurants based on number of orders.
     """
     return get_popular_restaurants_by_orders(limit)
+@router.get("/popular-restaurants/ratings")
+def popular_by_ratings(limit: int = Query(default=10, ge=1)):
+    """
+    Returns the most popular restaurants based on average rating.
+    """
+    return get_popular_restaurants_by_rating(limit)
