28885247cc68c7461ecc3391d89c94e271300cc4
Drea Akabo <agbebakuandrea1@gmail.com>
2026-03-22 07:52:54 -0700

    add popular restaurants route to main

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from app.routers.fulfillment import router as fulfillment_router
 from app.routers.menu_search import router as menu_search_router
 from app.routers.order_notification import router as order_notification_router
 from app.routers.statistics import router as statistics_router 
+from app.routers.popular_restaurants import router as popular_restaurants_router
 
 app = FastAPI(
     title="Gobbl Food Delivery API",
@@ -34,6 +35,7 @@ app.include_router(fulfillment_router)
 app.include_router(menu_search_router)
 app.include_router(order_notification_router)
 app.include_router(statistics_router)
+app.include_router(popular_restaurants_router)
 
 @app.get("/")
 def root():
