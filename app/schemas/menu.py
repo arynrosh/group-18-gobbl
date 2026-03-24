97fff1f030aea13b149c2111e5ffe62094e0bfc5
arynrosh <arynrosh@gmail.com>
2026-03-23 20:59:15 -0700

    extending menu.json and schemas to allow additional information about restaurant and food item (for integration), also added more items which loosely follow the given csv

--- app/schemas/menu.py
+++ app/schemas/menu.py
@@ -1,12 +1,27 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
+#defined but not used (yet)
+class MenuItem(BaseModel):
+    menu_item_id: int
+    restaurant_id: int
+    restaurant_name: str
+    cuisine: str
+    food_item: str
+    order_value: float
+    customer_rating: Optional[int] = None
+    delivery_time_actual: Optional[int] = None
+
+#commented out for the sake of not breaking existing code, will be reimplemented :)
 
 class MenuItemCreate(BaseModel):
+    # restaurant_name: str = Field(..., min_length=1, max_length=100)
+    # cuisine: str = Field(..., min_length=1, max_length=100)
     food_item: str = Field(..., min_length=1, max_length=100)
     order_value: float = Field(..., gt=0)
 
 class MenuItemUpdate(BaseModel):
+    # restaurant_name: str = Field(..., min_length=1, max_length=100)
+    # cuisine: str = Field(..., min_length=1, max_length=100)
     food_item: Optional[str] = Field(None, min_length=1, max_length=100)
     order_value: Optional[float] = Field(None, gt=0)
-
