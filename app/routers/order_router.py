caf92c4a4a42c5e456fc93c01ce0332c80ae8d50
ArceusCoding <aylaj884@gmail.com>
2026-03-23 17:39:17 -0700

    Added my files back in after rebasing from main

---
+++ app/routers/order_router.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends
+from app.services.order_service import addToOrder, removeFromOrder, sendOrder, getOrder, getOrderItem, updateStatus, completeOrderStatus
+from app.schemas.order import Order, OrderItem, Status
+from app.auth.dependencies import get_current_user
+    
+# Reference:
+# router = APIRouter(prefix="/menu", tags=["menu"])
+OrdeRouter = APIRouter(prefix="/orders", tags=["order_id"])
+foodRouter = APIRouter(prefix="/OrderItem", tags=["food_item"])
+statusRouter = APIRouter(prefix="/status", tags=["order_id"])
+
+@OrdeRouter.post("/{order_id}/")
+def createOrder(order_id: str, resturant_id: int, current_user: dict = Depends(get_current_user)):
+    return Order(order_id = order_id,
+        customer_id = current_user,
+        restaurant_id = resturant_id,
+        items = [],
+        sent = False)
+
+@OrdeRouter.get("/{order_id}/")
+def getOrder(order_id: str):
+    return getOrder(order_id)
+
+@OrdeRouter.put("/{order_id}/")
+def updateOrderAdd(order_id: str, food: str):
+    return addToOrder(order_id, food)
+
+@OrdeRouter.put("/{order_id}/")
+def updateOrderRemove(order_id: str, food: str):
+    return removeFromOrder(order_id, food)
+
+@OrdeRouter.put("/{order_id}/")
+def updateOrderSend(order_id: str):
+    return sendOrder(order_id)
+
+@foodRouter.get("/{food_item}/")
+def getFood(food_item: str):
+    return getOrderItem(food_item)
+
+@foodRouter.post("/{food_item}/")
+def createOrderItem(food_name: str, howMany: int, cost: float, resturant: int):
+    return OrderItem(food_item = food_name,
+    quantity = howMany,
+    order_value = cost,
+    resturant_id = resturant)
+
+@statusRouter.post("/{status}/")
+def createStatus(orderid: str):
+    return Status(order_id= orderid,
+                  current = "Sent",
+                  complete = False)
+
+@statusRouter.put("/{status}/")
+def updateStatusRout(orderid: str, msg: str):
+    return updateStatus(orderid, msg)
+
+@statusRouter.put("/{status}/")
+def completeStatusRout(orderid: str):
+    return completeOrderStatus(orderid)
---
+++ app/services/order_service.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import pytest
+from app.schemas.order import OrderItem, Order, Status
+
+App = FastAPI()
+
+#Task 4.1: Design database tables for Orders, Order Items, and Status Tracking
+#Task 4.2: Implement API to create, modify, and submit orders (cart system)
+#Task 4.3: Implement logic to prevent modifications after order completion
+#Task 4.4: Implement API to fetch order status for a customer or restaurant
+#Task 4.5: Unit tests for order creation, modification, and status updates-
+
+def addToOrder(order: Order, orderItem: OrderItem) -> Order:
+    if order.sent == False:
+        order.items.append(orderItem)
+    return order
+
+def removeFromOrder(order: Order, orderItem: OrderItem) -> Order:
+    if order.sent == False:
+            if orderItem in order.items:
+                order.items.remove(orderItem)
+    return order        
+
+def sendOrder(order: Order) -> Order:
+    order.sent = True
+
+def getOrderId(order: Order) -> Order:
+    return order.order_id
+
+def getOrderCustomer(order: Order) -> Order:
+    return order.customer_id
+
+def getOrderResturant(order: Order) -> Order:
+    return order.restaurant_id
+
+def getOrderItems(order: Order) -> Order:
+    return order.items
+
+def getOrderItem(food: OrderItem) -> OrderItem:
+    return food
+
+def getOrderSent(order: Order) -> Order:
+    return order.sent
+
+def getOrder(order: Order) -> Order:
+    Order_dict = [order.order_id, order.customer_id, order.restaurant_id, order.items, order.sent]
+    return Order_dict
+
+def updateStatus(status: Status, msg: str) -> Status:
+    if status.complete == False:
+        status.current = msg
+
+def completeOrderStatus(status: Status) -> Status:
+    status.complete = True
+
+def getStatusCurrent(status: Status) -> Status:
+    return status.current
+
+def getStatusComplete(status: Status) -> Status:
+    return status.complete
