98ef1eeb9654af0f16f797b04ae6e8e70e57e0dd
BarhaKainth <barhask14@gmail.com>
2026-03-25 13:31:33 -0700

    renamed driver_distance to delivery_distance, removed restaurant_id from Order, moved restaurant_id to top of OrderItem

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -2,50 +2,20 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class OrderItem(BaseModel):
-    """
-    Represents a signle item in an order.
-
-    Attributes:
-    - food_item (str) : name of the food item
-    - quantity (int): quantity ordered
-    -order_value (float): price per item
-    """
+    restaurant_id: int = Field(..., gt=0)
     food_item: str
-    quantity: int = Field(..., gt = 0)
-    order_value: float = Field(..., gt = 0)
-    restaurant_id: int = Field(..., gt = 0)
+    quantity: int = Field(..., gt=0)
+    order_value: float = Field(..., gt=0)
 
 class Order(BaseModel):
-    """
-    Represents a customer order. 
-
-    Attributes:
-    -  order_id (str): Unique identifier for the order
-    -  customer_id (str): ID of the customer placing the order
-    -  restaurant_id (int): ID of the restaurant being ordered from
-    -  items (List[OrderItem]): List of items in the order
-    """
-
     order_id: str
     customer_id: str
-    restaurant_id: int
-    driver_distance: int
-    assigned_driver_id: int
+    delivery_distance: int
+    assigned_driver_id: int | None = None
     items: List[OrderItem]
     sent: bool
 
-
 class CostBreakdown(BaseModel):
-    """
-    Represents the cost breakdown of an order.
-
-    Attributes:
-        order_id (str): The order this breakdown belongs to
-        subtotal (float): Sum of all item costs
-        tax (float): Tax applied to subtotal
-        delivery_fee (float): Flat delivery fee
-        total (float): Final total cost.
-    """
     order_id: str
     subtotal: float
     tax: float
@@ -53,14 +23,6 @@ class CostBreakdown(BaseModel):
     total: float
 
 class Status(BaseModel):
-    """
-    Used for communicating status of an order
-
-    Attributes:
-        order_id (str): The order that it is connected to
-        current (str): The current status of the order
-        complete (bool): Is the order complete or not
-    """
     order_id: str
     current: str
     complete: bool
